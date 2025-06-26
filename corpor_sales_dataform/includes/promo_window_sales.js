812563bfde7d8ff5c0ad849e240e842df66e0ca5
Andy Song <congxisong@hotmail.com>
2025-06-26 12:51:14 +0100

    sales data with promo on rolling dates done

---
+++ corpor_sales_dataform/includes/promo_window_sales.js
@@ -0,0 +1,11 @@
+function promo_window(targetDate,window_size) {
+    const startDate = `DATE_SUB(DATE '${targetDate}',INTERVAL ${window_size} DAY)`;
+    const endDate = `DATE_SUB(DATE '${targetDate}',INTERVAL 1 DAY)`;
+    
+    return `AVG(CASE WHEN onpromotion > 0 AND date BETWEEN
+                ${startDate} AND ${endDate}  
+                THEN unit_sales
+                ELSE NULL
+                END) AS has_promo_mean_${window_size}`;
+}
+module.exports = promo_window;
