f595bbeef377f3846460412e26671244c479c9dd
Andy Song <congxisong@hotmail.com>
2025-06-29 15:36:29 +0100

    fix the group issue in js file

--- corpor_sales_dataform/includes/promo_window_sales.js
+++ corpor_sales_dataform/includes/promo_window_sales.js
@@ -1,23 +1,12 @@
-function promo_window(targetDate,window_size) {
+function promo_window(targetDate,window_size,promoCond) {
     const startDate = `DATE_SUB(DATE '${targetDate}',INTERVAL ${window_size} DAY)`;
     const endDate = `DATE_SUB(DATE '${targetDate}',INTERVAL 1 DAY)`;
     
-    return `AVG(CASE WHEN onpromotion > 0 AND date BETWEEN
+    return `AVG(CASE WHEN onpromotion ${promoCond} AND date BETWEEN
                 ${startDate} AND ${endDate}  
                 THEN unit_sales
                 ELSE NULL
                 END) AS has_promo_mean_${window_size}`;
 }
 
-function no_promo_window(targetDate,window_size) {
-    const startDate = `DATE_SUB(DATE '${targetDate}',INTERVAL ${window_size} DAY)`
-    const endDate = `DATE_SUB(DATE '${targetDate}',INTERVAL 1 DAY)`
-    return `
-          AVG(CASE WHEN onpromotion=0 AND date between ${startDate} AND ${endDate}
-               THEN unit_sales
-               ELSE NULL
-               END) AS no_promo_sales_${window_size}
-        `;
-}
-module.exports = { promo_window,
-                   no_promo_window };
+module.exports = { promo_window };
