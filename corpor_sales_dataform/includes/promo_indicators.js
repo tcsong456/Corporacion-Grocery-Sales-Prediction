0658a9438a0aa29c182fabef53ff8bbee3050bbc
Andy Song <congxisong@hotmail.com>
2025-06-30 15:11:43 +0100

    rolling promo sums done

--- corpor_sales_dataform/includes/promo_indicators.js
+++ corpor_sales_dataform/includes/promo_indicators.js
@@ -1,40 +1,26 @@
 function promo_window_sum(targetDate,windows,keyCols,refTable) {
-    const windowList = `[${ windows.join(",") }]`;
-    const dynamicSelectCols = keyCols.map(k => `b.${k}`).join(",")
-    const pivotAlias = windows.map(w => {
-                                        const alias = w >=0 ? `past_${w}` : `future_${Math.abs(w)}`;
-                                        return `${w} AS ${alias}`;
-                }).join(",");
-    return    `
-                WITH date_windows AS (
-                SELECT
-                DATE('${targetDate}') AS target_date,
-                w AS window_size,
-                CASE WHEN  w >= 0 THEN DATE_SUB(DATE('${targetDate}'),INTERVAL w DAY)
-                                  ELSE DATE_SUB(DATE('${targetDate}'),INTERVAL 1 DAY) END AS window_start,
-                CASE WHEN w >= 0 THEN DATE_SUB(DATE('${targetDate}'),INTERVAL 1 DAY)
-                                 ELSE DATE_ADD(DATE('${targetDate}'),INTERVAL ABS(w) DAY) END AS window_end
-                FROM UNNEST(${windowList}) AS w
-                ),
-                df AS(
-                SELECT
-                ${dynamicSelectCols},
-                b.onpromotion,
-                a.window_size
-                FROM
-                date_windows AS a JOIN
-                ${refTable} AS b ON
-                b.date BETWEEN a.window_start AND a.window_end
-                )
-                SELECT
-                *
-                FROM df
-                PIVOT(
-                SUM(onpromotion) AS promo_sum,
-                AVG(onpromotion) AS promo_mean
-                FOR window_size IN (${pivotAlias})
-                )
-                `;
+    const groupCols = keyCols.join(",")
+    const columns = windows.flatMap(w => {
+                                        const suffix = w >= 0 ? `past_${w}` : `future_${Math.abs(w)}`;
+                                        const window_start = w >= 0 ? `DATE_SUB(DATE('${targetDate}'),INTERVAL ${w} DAY)` :
+                                                                      `DATE_SUB(DATE('${targetDate}'),INTERVAL 1 DAY)`;
+                                        const window_end = w >= 0 ? `DATE_SUB(DATE('${targetDate}'),INTERVAL 1 DAY)` :
+                                                                    `DATE_ADD(DATE('${targetDate}'),INTERVAL ${Math.abs(w)} DAY)`;
+                                        const columns = [`SUM(CASE WHEN date BETWEEN ${window_start} AND ${window_end}
+                                                         THEN onpromotion ELSE 0 END) AS promo_sum_${suffix}`];
+                                        if (w>=0) {
+                                            columns.push(`SUM(CASE WHEN date BETWEEN ${window_start} AND ${window_end}
+                                                          THEN onpromotion ELSE 0 END) / ${Math.abs(w)} AS promo_mean_${suffix}`);
+                                        }
+                                        return columns;
+                                                
+    }).join(",\n")
+    return `SELECT
+            ${groupCols},
+            ${columns}
+            FROM ${refTable}
+            GROUP BY ${groupCols}
+            `
 };
 
 module.exports = { promo_window_sum }
