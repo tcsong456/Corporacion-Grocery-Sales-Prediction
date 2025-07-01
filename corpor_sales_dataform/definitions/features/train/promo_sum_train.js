590514541ecf1a8098967636b0e8868ed81b9b73
Andy Song <congxisong@hotmail.com>
2025-07-01 15:01:22 +0100

    create item agg dataset

--- corpor_sales_dataform/definitions/features/train/promo_sum_train.js
+++ corpor_sales_dataform/definitions/features/train/promo_sum_train.js
@@ -1,4 +1,4 @@
-const { promo_window_sum } = require("includes/promo_indicators");
+const { promo_window_sum } = require("includes/promo_window_sum");
 const windows = [-3,-7,-14,7,14,30,60,140];
 const dates = ['2017-6-14','2017-6-21','2017-6-28','2017-7-05','2017-7-12','2017-7-19'];
 const groups = [["item_nbr"],["class","store_nbr"],["store_nbr","item_nbr"]];
