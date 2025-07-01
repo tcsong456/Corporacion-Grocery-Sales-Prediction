6b49ea0c81eaf17cafe96d40f6d2a86efa08880a
Andy Song <congxisong@hotmail.com>
2025-07-01 16:16:10 +0100

    promor sales dataset validation done

--- corpor_sales_dataform/includes/promo_window_sales.js
+++ corpor_sales_dataform/includes/promo_window_sales.js
@@ -1,12 +1,13 @@
 function promo_window(targetDate,window_size,promoCond) {
     const startDate = `DATE_SUB(DATE '${targetDate}',INTERVAL ${window_size} DAY)`;
     const endDate = `DATE_SUB(DATE '${targetDate}',INTERVAL 1 DAY)`;
+    const colPrefix = promoCond===">0" ? "has_promo" : "no_promo";
     
     return `AVG(CASE WHEN onpromotion ${promoCond} AND date BETWEEN
                 ${startDate} AND ${endDate}  
                 THEN unit_sales
                 ELSE NULL
-                END) AS has_promo_mean_${window_size}`;
+                END) AS ${colPrefix}_mean_${window_size}`;
 }
 
 module.exports = { promo_window };
