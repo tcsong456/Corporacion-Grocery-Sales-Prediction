20420e872cded9f601def60b4c52b485c7839120
Andy Song <congxisong@hotmail.com>
2025-07-03 19:50:54 +0100

    df_validation done re-committed since some files were missing at staging area

---
+++ corpor_sales_dataform/definitions/features/valid/promo_sales_data_valid.js
@@ -0,0 +1,23 @@
+const { promo_window } = require("includes/promo_window_sales");
+const windows = [3,7,14,30,60,140];
+const date = '2017-7-26';
+const groups = [["item_nbr"],["class","store_nbr"],["store_nbr","item_nbr"]];
+const promo_conditions = [">0","=0"]
+
+groups.forEach(group => {
+    const keyCols = group.join(", ");
+    const view_prefix = group.map(x => x.replace(/_nbr$/,'')).join('_');
+    const refTable = `partitioned_${view_prefix}_data`;
+    promo_conditions.forEach(promoCondition => {
+        const promoFlag = promoCondition === ">0" ? "has_promo" : "no_promo";
+        const viewName = `${view_prefix}_${promoFlag}_sales_sum_${date.replace(/-/g,'')}`;
+        const columns = windows.map(w => promo_window(date,w,promoCondition,view_prefix)).join(",\n")
+        publish(viewName,{type: "view"}).query(ctx =>`
+                                                    SELECT
+                                                    ${keyCols},
+                                                    ${columns}
+                                                    FROM ${ctx.ref(refTable)}
+                                                    GROUP BY ${keyCols}
+                                                `);
+    });
+});
---
+++ corpor_sales_dataform/definitions/features/valid/rolling_sales_stats_valid.js
@@ -0,0 +1,15 @@
+const { rolling_window_stats } = require("includes/sales_window_stats");
+const windows = [3,7,14,30,60,140];
+const date = '2017-7-26';
+const groups = [["item_nbr"],["class","store_nbr"],["store_nbr","item_nbr"]];
+
+groups.forEach(group => {
+    const view_prefix = group.join("_").replace(/_nbr/g,"");
+    const refTable = `partitioned_${view_prefix}_data`;
+    const viewName = `${view_prefix}_rolling_sales_stats_${date.replace(/-/g,'')}`;
+    publish(viewName,{type: "view"}).query(ctx => 
+                                                rolling_window_stats(date,windows,0.9,
+                                                group,
+                                                ctx.ref(refTable),view_prefix)
+                          );
+});
