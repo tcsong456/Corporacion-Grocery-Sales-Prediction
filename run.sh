70146ed825c16d46930610e20345a4767ded745a
Andy Song <congxisong@hotmail.com>
2025-07-11 18:51:15 +0100

    commit before ci-cd pipeline

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 bash shell_scripts/gen_auth_key.sh
-docker build -f dockerfiles/terraform_setup_dockerfile -t data_download_and_upload:v1 .
-docker run data_download_and_upload:v1
+docker build -f dockerfiles/terraform_setup_dockerfile -t data_download_and_creation .
+docker run data_download_and_creation
+docker build -f dockerfiles/dataform_dockerfile -t dataform_models .
+docker run dataform_models
