0eca0ec53ccad6a695621997e25bec7bc68c2769
Andy Song <congxisong@hotmail.com>
2025-07-12 19:18:22 +0100

    final_process.py passed flake8 linting test

--- data_preprocess/final_process.py
+++ data_preprocess/final_process.py
@@ -6,8 +6,8 @@ Created on Mon Jun 16 16:42:56 2025
 """
 from google.cloud import storage
 from pyspark.sql import SparkSession
-from pyspark.sql.types import ByteType,ShortType,IntegerType
-from pyspark.sql.functions import col,expr,to_date
+from pyspark.sql.types import ByteType, ShortType, IntegerType
+from pyspark.sql.functions import col, expr, to_date
 
 bucket_name = 'corpor-sales-data'
 df_sales_bucket = "gs://" + bucket_name + "/full_df/"
@@ -22,53 +22,56 @@ spark.sparkContext.setLogLevel("ERROR")
 df_sales = spark.read.parquet(df_sales_bucket)
 df_promo_pivot = spark.read.parquet(df_promo_bucket)
 columns = df_promo_pivot.columns
-columns = [c for c in columns if c not in ['store_nbr','item_nbr']]
-stack_expr = f"stack({len(columns)},"+",".join(f"'{c}',`{c}`" for c in columns)+") as (date,onpromotion)"
-df_long = df_promo_pivot.select('store_nbr','item_nbr',expr(stack_expr))
-df_long = df_long.withColumn('item_nbr',col('item_nbr').cast(IntegerType())) \
-                 .withColumn('store_nbr',col('store_nbr').cast(ByteType())) \
-                 .withColumn('onpromotion',col('onpromotion').cast(ByteType())) \
-                 .withColumn('date',to_date(col('date'),'yyyy-MM-dd'))
+columns = [c for c in columns if c not in ['store_nbr', 'item_nbr']]
+stack_expr = f"stack({len(columns)}," + ",".join(f"'{c}',`{c}`" for c in columns) + ") as (date,onpromotion)"
+df_long = df_promo_pivot.select('store_nbr', 'item_nbr', expr(stack_expr))
+df_long = df_long.withColumn('item_nbr', col('item_nbr').cast(IntegerType())) \
+                 .withColumn('store_nbr', col('store_nbr').cast(ByteType())) \
+                 .withColumn('onpromotion', col('onpromotion').cast(ByteType())) \
+                 .withColumn('date', to_date(col('date'), 'yyyy-MM-dd'))
 df_long.write \
-  .format('parquet') \
-  .mode('overwrite') \
-  .save("gs://" + bucket_name + "/df_promo_long/")
+    .format('parquet') \
+    .mode('overwrite') \
+    .save("gs://" + bucket_name + "/df_promo_long/")
 
 items = spark.read \
-              .format('csv') \
-              .option('header',True) \
-              .option('inferschema',True) \
-              .load(item_bucket)
+    .format('csv') \
+    .option('header', True) \
+    .option('inferschema', True) \
+    .load(item_bucket)
 stores = spark.read \
               .format('csv') \
-              .option('header',True) \
-              .option('inferschema',True) \
+              .option('header', True) \
+              .option('inferschema', True) \
               .load(store_bucket)
-df_sales = df_sales.join(items,
-              on=['item_nbr'],
-              how='left') \
-        .join(stores,
-              on=['store_nbr'],
-              how='left')
+df_sales = (
+    df_sales.join(
+        items,
+        on=['item_nbr'],
+        how='left'
+    ).join(
+        stores,
+        on=['store_nbr'],
+        how='left'
+    )
+)
 
-df_sales = df_sales.withColumn('cluster',col('cluster').cast(ByteType())) \
-        .withColumn('class',col('class').cast(ShortType())) \
-        .withColumn('perishable',col('perishable').cast(ByteType())) \
-        .withColumn('store_nbr',col('store_nbr').cast(ByteType())) \
-        .withColumn('item_nbr',col('item_nbr').cast(IntegerType()))
+df_sales = df_sales.withColumn('cluster', col('cluster').cast(ByteType())) \
+    .withColumn('class', col('class').cast(ShortType())) \
+    .withColumn('perishable', col('perishable').cast(ByteType())) \
+    .withColumn('store_nbr', col('store_nbr').cast(ByteType())) \
+    .withColumn('item_nbr', col('item_nbr').cast(IntegerType()))
 
 df_sales.write \
-  .format('parquet') \
-  .mode('overwrite') \
-  .save(output_bucket)
-  
+    .format('parquet') \
+    .mode('overwrite') \
+    .save(output_bucket)
+
 client = storage.Client()
 bucket = client.bucket(bucket_name)
-for blob_name in ['stores.csv','oil.csv','transactions.csv','holidays_events.csv']:
+for blob_name in ['stores.csv', 'oil.csv', 'transactions.csv', 'holidays_events.csv']:
     blob = bucket.blob(blob_name)
     blob.delete()
 blobs = bucket.list_blobs(prefix='full_df/')
 for blob in blobs:
     blob.delete()
-
-
