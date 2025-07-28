d12dcef35205ca85ad930b25be995b93639a4bcc
Andy Song <congxisong@hotmail.com>
2025-07-28 21:53:00 +0100

    added df persist for caching

--- tests/test_gcs_io.py
+++ tests/test_gcs_io.py
@@ -1,12 +1,8 @@
-# import os
 import uuid
 import time
 from google.cloud import storage
 from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StructField, IntegerType, ByteType
-
-# key_path = os.path.join(os.path.dirname(__file__), '..', 'key.json')
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = key_path
 
 
 def create_bucket_and_upload_data(bucket_name, blob_name, data_path):
@@ -31,25 +27,6 @@ def compute_ordered_hash(df):
 
 
 def test_gcs_read_write(spark):
-    # import os
-    # import logging
-    # log = logging.getLogger(__name__)
-    # hadoop_ver = gcs_spark._jvm.org.apache.hadoop.util.VersionInfo.getVersion()
-    # log.info("Hadoop: %s", hadoop_ver)
-    # log.info("GCS connector present: %s", os.path.exists("/opt/jars/gcs-connector.jar"))
-    # log.info("GCS connector present on disk: %s", os.path.exists("/opt/jars/gcs-connector-hadoop3-2.2.17.jar"))
-    # try:
-    #     spark._jvm.java.lang.Class.forName(
-    #         "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem"
-    #     )
-    #     log.info("GCS connector class is on the classpath")
-    # except Exception as e:
-    #     log.info(f"GCS connector class NOT found:{e}")
-    # log.info("spark.jars =", spark.sparkContext.getConf().get("spark.jars", ""))
-    # hc = spark._jsc.hadoopConfiguration()
-    # log.info("fs.gs.impl =", hc.get("fs.gs.impl"))
-    # log.info("fs.AbstractFileSystem.gs.impl =", hc.get("fs.AbstractFileSystem.gs.impl"))
-
     create_bucket_and_upload_data('integration_test_io', 'integration_test.csv', 'data/test.csv')
     test_id = uuid.uuid4().hex[:8]
     input_path = "gs://integration_test_io/integration_test.csv"
@@ -79,6 +56,8 @@ def test_gcs_read_write(spark):
         .withColumn(
             'promo_sum', F.col('promo_sum').cast(IntegerType())
             )
+    df_promo_sum = df_promo_sum.persist()
+    df_promo_sum.count()
     df_test_schema = StructType([
         StructField('store_nbr', ByteType()),
         StructField('item_nbr', IntegerType()),
