43a69978936d25a5d498072e958cab80841a9071
Andy Song <congxisong@hotmail.com>
2025-08-01 19:14:24 +0100

    use secrets.credentials for auth purpose instead of exposing the key

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,13 @@
 provider "google-beta" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file("${path.module}/../key.json")
+  project = var.project_id
+  region  = var.region
+  #  credentials = file("${path.module}/../key.json")
+  credentials = var.credential_key_json
 }
 
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file("${path.module}/../key.json")
+  project = var.project_id
+  region  = var.region
+  #  credentials = file("${path.module}/../key.json")
+  credentials = var.credential_key_json
 }
