356ea68fe8c527e956ddfec15c0796fadadea1d9
Andy Song <congxisong@hotmail.com>
2025-08-01 17:58:29 +0100

    added terraform-ci

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
 provider "google-beta" {
-  project      = var.project_id
-  region       = var.region
-  credentials  = file("${path.module}/../key.json")
+  project     = var.project_id
+  region      = var.region
+  credentials = file("${path.module}/../key.json")
 }
 
 provider "google" {
-  project      = var.project_id
-  region       = var.region
-  credentials  = file("${path.module}/../key.json")
+  project     = var.project_id
+  region      = var.region
+  credentials = file("${path.module}/../key.json")
 }
