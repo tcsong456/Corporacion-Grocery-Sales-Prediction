bd5d5a1f9d2dd95145034a2871cb032592740dff
Andy Song <congxisong@hotmail.com>
2025-08-02 12:52:36 +0100

    stated env varaibles for main.tf

--- shell_scripts/run_terraform.sh
+++ shell_scripts/run_terraform.sh
@@ -5,19 +5,16 @@ GCP_REGION="europe-west1"
 UMSA="corpor-sales-sa"
 
 cd $(pwd)/terraform 
-#cd terraform
 sed -i 's/\r$//' main.tf
 terraform init
 
 terraform plan \
   -var="project_id=${PROJECT_ID}" \
   -var="umsa=${UMSA}" \
-  -var="region=${GCP_REGION}" \
-  -var="project_nbr=${PROJECT_NBR}"
+  -var="region=${GCP_REGION}" 
 
 terraform apply \
   -var="project_id=${PROJECT_ID}" \
   -var="umsa=${UMSA}" \
-  -var="region=${GCP_REGION}" \
-  -var="project_nbr=${PROJECT_NBR}" \
+  -var="region=${GCP_REGION}"
   --auto-approve >> provisioning.output
